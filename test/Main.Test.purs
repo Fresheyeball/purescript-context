193350f38ee4706f7cbaae6bdedd5fc197c6a7c0
Isaac Shapira <fresheyeball@gmail.com>
2014-12-15 01:00:30 -0700

    pretty

--- test/Main.Test.purs
+++ test/Main.Test.purs
@@ -1,31 +1,21 @@
 module Main where
 
 import Context
-import Test.Mocha
-import Test.Chai
+import Test.Mocha(describe, it)
+import Test.Chai(expect, toEqual)
 import Data.Foreign.OOFFI(getter)
 import Control.Monad.Eff
 
-murf               = "murf"
+murf                       = "murf"
 foreign import attachMurfToContext """
-  var murf = "murf";
-  var attachMurfToContext = null;
-  try{
-    window[murf] = murf;
-    console.log("context is window");
-  }catch(e){
-    try{
-      global[murf] = murf;
-      console.log("context is global");
-    }catch(e){
-      self[murf] = murf;
-      console.log("context is self");
-    }
+  var murf                 = "murf",
+      attachMurfToContext  =  null;
+  try{        window[murf] =  murf; console.log("context is window"); }
+  catch(e){
+    try{      global[murf] =  murf; console.log("context is global"); }
+    catch(e){ self  [murf] =  murf; console.log("context is self");   }
   }
 """ :: Unit
 
-getMurf :: forall e. Context -> Eff e String
-getMurf = getter murf 
-
 main = describe "Context" <<< it murf 
-     $ getContext >>= getMurf >>= \murf' -> expect murf' `toEqual` murf
+     $ getContext >>= getter murf >>= \murf' -> expect murf' `toEqual` murf
