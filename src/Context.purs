e41aa548dcb29ad2e11e290e66d806c7a2bacc8e
Isaac Shapira <fresheyeball@gmail.com>
2014-12-15 01:20:01 -0700

    more efficient

--- src/Context.purs
+++ src/Context.purs
@@ -5,10 +5,8 @@ import Control.Monad.Eff
 foreign import data Context :: *
 
 foreign import getContext """
-  function getContext(){
-    var context;
-    try      { context = Function('return this')() || (42, eval)('this'); } 
-    catch(e) { context = window; }    
-    return context;
-  }
+  var context;
+  try      { context = Function('return this')() || (42, eval)('this'); } 
+  catch(e) { context = window; }    
+  function getContext(){ return context; }
 """ :: forall e. Eff e Context
